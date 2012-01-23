a33d8ff61477dc9e4ec412700a0c0bd19a82b19a
liutanyu <liutanyu@gmail.com>
2012-01-23 00:19:02 +0000

    workable implementation

--- ifnet.m
+++ ifnet.m
@@ -7,20 +7,19 @@
  tau = 10;       % time constant [ms]
  E_L = -65*ones(m, m);      % resting potential [mV]
  theta = -55;    % firing threshold [mV]
- %RI_ext = input('input current');     % constant external input [mA/Ohm]
  
 %%
    
    X = zeros(m,m);
    
    p = -1:1;
-   for count=1:50,
+   for count=1:500,
       kx=floor(rand*(m-4))+2; 
       ky=floor(rand*(m-4))+2; 
       X(kx+p,ky+p)=(rand(3)>0.1);
    end;
    
-   X = (X ~= 0).* (-55);
+   X = (X ~= 0) .* (-55); + (X == 0) .* (-65);
    spike = X ~= 0;
    
    % The "find" function returns the indices of the nonzero elements.
@@ -28,7 +27,7 @@
    
    z = i.*0;
    
-  % figure(gcf);
+
    plot3(i,j,z,'.', ...
       'Color','blue', ...
       'MarkerSize',12);
@@ -46,7 +45,10 @@
    s = [2:m 1];
    w = [m 1:m-1];
  
-   for t = 1:1:50;
+   X = (X ~= 0) .* (-55) + (X == 0) .* (-65);
+   
+   for t = 1:1:3;
+       
       % How many of eight neighbors are alive.
       spike = spike(n,:) + spike(s,:) + spike(:,e) + spike(:,w) + ...
          spike(n,e) + spike(n,w) + spike(s,e) + spike(s,w);
@@ -54,17 +56,15 @@
       % A live cell with two live neighbors, or any cell with three
       % neigbhors, is alive at the next time step.
       
-      RI_ext = spike .* 10;
-      
-      X = (X-dt/tau.*((X-E_L)-RI_ext));
+      RI_ext = spike .* 100;
+   
+      X = X - ((dt/tau) .* ones(m, m)) .* ((X - E_L) - RI_ext);
       
       temp1 = X > -55;
       
       X = ~temp1 .* X + temp1 .* -65;
       
-      temp2 = X ~= 0;
-      
-      spike = temp1 .* temp2;
+      spike = temp1;
       
       % Update plot.
       [i,j] = find(spike);
