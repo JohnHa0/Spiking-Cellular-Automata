a7e6ccc539bad60a0aaef9963d17ff048a1854e4
liutanyu <liutanyu@gmail.com>
2012-01-23 00:44:12 +0000

    work right

--- ifnet.m
+++ ifnet.m
@@ -1,33 +1,38 @@
+%% This program can display the fired neurons of a 2D neural network in 3D 
+%% the thired dimenstion is time.
+
+
+
 %% Simulation of (leaky) integrate-and-fire neuron
  clear; clf;
 
 %% parameters of the model
- m = 101;
- dt = 0.1;       % integration time step [ms]
- tau = 10;       % time constant [ms]
- E_L = -65*ones(m, m);      % resting potential [mV]
- theta = -55;    % firing threshold [mV]
+ m = 101;                    % there will have m*m neurons
+ dt = 0.1;                   % integration time step [ms]
+ tau = 10;                   % time constant [ms]
+ E_L = -65*ones(m, m);       % resting potential [mV]
+ theta = -55;                % firing threshold [mV]
  
-%%
+%% make neural network with initial value
    
    X = zeros(m,m);
-   
+   % set the initial value using rand()
    p = -1:1;
    for count=1:500,
       kx=floor(rand*(m-4))+2; 
       ky=floor(rand*(m-4))+2; 
       X(kx+p,ky+p)=(rand(3)>0.1);
    end;
-   
-   X = (X ~= 0) .* (-55); + (X == 0) .* (-65);
+   % if the value of X is not 0, the this neuron fired, else set it to -65 in line 48 
+   X = (X ~= 0) .* (-55);
    spike = X ~= 0;
    
    % The "find" function returns the indices of the nonzero elements.
    [i,j] = find(spike);
    
-   z = i.*0;
+   z = i.*0;        % z is the time axes
    
-
+   % plot the fire neuron  
    plot3(i,j,z,'.', ...
       'Color','blue', ...
       'MarkerSize',12);
@@ -45,25 +50,27 @@
    s = [2:m 1];
    w = [m 1:m-1];
  
-   X = (X ~= 0) .* (-55) + (X == 0) .* (-65);
+   X = (X ~= 0) .* (-55) + (X == 0) .* (-65); % set the unfired neuron to -65
    
+   for t = 1:1:10;
        
-      % How many of eight neighbors are alive.
+      % how many of eight neighbors are fired.
       spike = spike(n,:) + spike(s,:) + spike(:,e) + spike(:,w) + ...
          spike(n,e) + spike(n,w) + spike(s,e) + spike(s,w);
       
-      % A live cell with two live neighbors, or any cell with three
-      % neigbhors, is alive at the next time step.
+      % the fire neuron will generat 80mV current.
+      % the NO. of neurons can be stable when the current value is around 150
       
-      RI_ext = spike .* 100;
+      
+      RI_ext = spike .* 80;
    
-      X = X - ((dt/tau) .* ones(m, m)) .* ((X - E_L) - RI_ext);
+      X = X - ((dt/tau) .* ones(m, m)) .* ((X - E_L) - RI_ext);   % if equation
       
       temp1 = X > -55;
       
-      X = ~temp1 .* X + temp1 .* -65;
+      X = ~temp1 .* X + temp1 .* -65; % reset 
       
-      spike = temp1;
+      spike = temp1;    % reset the spike NO. so that spike can indicate which neuron is fired
       
       % Update plot.
       [i,j] = find(spike);
