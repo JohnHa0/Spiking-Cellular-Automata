b22762b0414bae6ca2824737639efa63547754d9
liutanyu <liutanyu@gmail.com>
2012-02-05 21:43:50 +0000

    add a popmenu

--- ifgui.m
+++ ifgui.m
@@ -22,7 +22,7 @@ function varargout = ifgui(varargin)
 
 % Edit the above text to modify the response to help ifgui
 
-% Last Modified by GUIDE v2.5 05-Feb-2012 11:55:31
+% Last Modified by GUIDE v2.5 05-Feb-2012 21:33:02
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -440,3 +440,442 @@ if get(handles.linebutton,'Value') == 1
 else
     axis off;
 end
+
+
+% --- Executes on selection change in popupmenu.
+function popupmenu_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu
+
+%%
+list=cellstr(get(hObject,'String'));
+pattrn=0;
+switch list{get(hObject,'value')}
+    case 'Random'
+        qst=questdlg('What density do you want?','Random','Low','Mid','High','Low');
+        switch qst
+            case 'Low'
+                d=.75;
+            case 'Mid'
+                d=.5;
+            case 'High'
+                d=.25;
+        end %switch qst
+        handles.vis_grid=rand(handles.M)>d;
+    case 'Blinker'
+        handles.pattern=blinker;
+        pattrn=1;
+    case 'Toad'
+        handles.pattern=toad;
+        pattrn=1;
+    case 'Beacon'
+        handles.pattern=beacon;
+        pattrn=1;
+    case 'Pulsar'
+        handles.pattern=pulsar;
+        pattrn=1;
+    case 'Lindner''s Oscillator'  %I don't familiar with this oscillator, so I called it by my name... :) 
+        handles.pattern=lindners;
+        pattrn=1;        
+    case 'Glider'
+        handles.pattern=glider;
+        pattrn=1;
+    case 'Lightweight Spaceship'
+        handles.pattern=LWSS;
+        pattrn=1;
+    case 'Coe Ship'
+        handles.pattern=coe_ship;
+        pattrn=1;
+    case 'The Turtle'
+        handles.pattern=turtle;
+        pattrn=1;
+    case 'The Schick Engine'
+        handles.pattern=schick;
+        pattrn=1;
+    case 'Holzwart Ship'
+        handles.pattern=holzwart;
+        pattrn=1;
+    case 'The Weekender'
+        handles.pattern=weekender;
+        pattrn=1;        
+    case 'Hickerson 13491'
+        handles.pattern=hickerson13491;
+        pattrn=1; 
+    case 'Hickerson 13881'
+        handles.pattern=hickerson13881;
+        pattrn=1;         
+    case 'Glider 17361'
+        handles.pattern(25:-1:1,25:-1:1)=glider17361;
+        pattrn=1;        
+    case 'The Spider'
+        handles.pattern=spider;
+        pattrn=1;
+    case 'Glider Gun'
+        handles.pattern=zeros(38);
+        handles.pattern(16:26,:)=glider_gun;
+        pattrn=1;
+    case 'Gun Fight 1'
+        handles.pattern=zeros(58);
+        a=glider_gun;
+        handles.pattern(5:15,1:38)=a;
+        handles.pattern(44:54,21:58)=a(end:-1:1,end:-1:1);
+        pattrn=1;
+    case 'Gun Fight 2'
+        handles.pattern=zeros(58);
+        a=glider_gun;
+        handles.pattern(8:18,1:38)=a;
+        handles.pattern(41:51,21:58)=a(end:-1:1,end:-1:1);
+        pattrn=1;
+    case 'Gun Fight 3'
+        handles.pattern=zeros(58);
+        a=glider_gun;
+        handles.pattern(12:22,1:38)=a;
+        handles.pattern(37:47,21:58)=a(end:-1:1,end:-1:1);
+        pattrn=1;
+    case 'Empty Square'
+        s=round(str2num(cell2mat(inputdlg('Enter side size','Square'))));
+        if isempty(s)|(s<2)|(s>handles.M)
+            h=errordlg(['Input must be a number between 2-',num2str(handles.M)],'Error');
+            waitfor(h);
+        else
+            handles.vis_grid=zeros(handles.M);
+            handles.vis_grid(1+round((handles.M-s)*.5):end-round((handles.M-s)*.5),1+round((handles.M-s)*.5))=1;
+            handles.vis_grid(1+round((handles.M-s)*.5):end-round((handles.M-s)*.5),end-round((handles.M-s)*.5))=1;
+            handles.vis_grid(1+round((handles.M-s)*.5),1+round((handles.M-s)*.5):end-round((handles.M-s)*.5))=1;
+            handles.vis_grid(end-round((handles.M-s)*.5),1+round((handles.M-s)*.5):end-round((handles.M-s)*.5))=1;
+        end
+    case 'Full Square'
+        s=round(str2num(cell2mat(inputdlg('Enter side size','Square'))));
+        if isempty(s)|(s<2)|(s>handles.M)
+            h=errordlg(['Input must be a number between 2-',num2str(handles.M)],'Error');
+            waitfor(h);
+        else
+            handles.vis_grid=zeros(handles.M);
+            handles.vis_grid(1+round((handles.M-s)*.5):end-round((handles.M-s)*.5),1+round((handles.M-s)*.5):end-round((handles.M-s)*.5))=1;
+        end
+    case 'Empty Circle'
+        s=round(str2num(cell2mat(inputdlg('Enter diameter size','Circle'))));
+        if isempty(s)|(s<2)|(s>handles.M)
+            h=errordlg(['Input must be a number between 2-',num2str(handles.M)],'Error');
+            waitfor(h);
+        else
+            [x,y]=meshgrid(linspace(-handles.M/2,handles.M/2,handles.M));
+            a=sqrt(x.^2+y.^2);
+            handles.vis_grid=zeros(handles.M);
+            handles.vis_grid( (a<=s/2) & (a>(s/2 -1)) )=1;
+        end                
+    case 'Full Circle'
+        s=round(str2num(cell2mat(inputdlg('Enter diameter size','Circle'))));
+        if isempty(s)|(s<2)|(s>handles.M)
+            h=errordlg(['Input must be a number between 2-',num2str(handles.M)],'Error');
+            waitfor(h);
+        else
+            [x,y]=meshgrid(linspace(-handles.M/2,handles.M/2,handles.M));
+            a=sqrt(x.^2+y.^2);
+            handles.vis_grid=zeros(handles.M);
+            handles.vis_grid(a<=(1+s/2))=1;
+        end        
+    case 'Diagonal'
+        handles.vis_grid=zeros(handles.M);
+        handles.vis_grid(1:4:end,1:4:end)=1;
+        handles.vis_grid(2:4:end,2:4:end)=1;
+        handles.vis_grid(3:4:end,3:4:end)=1;
+        handles.vis_grid(4:4:end,4:4:end)=1;
+    case 'Chess board'
+        handles.vis_grid=zeros(handles.M);
+        handles.vis_grid(1:2:end,1:2:end)=1;
+        handles.vis_grid(2:2:end,2:2:end)=1;
+    case '+ shape'
+        handles.vis_grid=zeros(handles.M);
+        handles.vis_grid(:,round(handles.M./2))=1;
+        handles.vis_grid(round(handles.M./2),:)=1;
+    case '# shape'
+        handles.vis_grid=zeros(handles.M);
+        handles.vis_grid(:,[ceil(handles.M./3),ceil(2*handles.M./3)])=1;
+        handles.vis_grid([ceil(handles.M./3),ceil(2*handles.M./3)],:)=1;
+    case 'X shape'
+        handles.vis_grid=eye(handles.M);
+        handles.vis_grid=(handles.vis_grid+handles.vis_grid(:,end:-1:1))>0;
+end
+if pattrn
+    handles=creat_pattern(handles);
+end
+guidata(hObject, handles);
+plot_grid(handles)
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+%%
+function [handles] = calc_game(handles)
+sum_grid=zeros(handles.M+4);
+bounded_grid=zeros(handles.M+6);
+bounded_grid(2:end-1,2:end-1)=handles.calc_grid;
+for k=-1:1
+    for l=-1:1
+        if ~((k==0)&(l==0))
+            sum_grid=sum_grid+bounded_grid(2+k:end-1+k,2+l:end-1+l);
+        end %if
+    end % for l
+end %for k
+next_grid=zeros(handles.M+4);
+next_grid((sum_grid==2)&(handles.calc_grid==1))=1;
+next_grid(sum_grid==3)=1;
+handles.calc_grid=next_grid;
+
+
+function [a]=blinker();
+a=zeros(5);
+a(3,2:4)=1;
+
+function [a]=toad();
+a=zeros(6);
+a(3,2:4)=1;
+a(4,3:5)=1;
+
+function [a]= beacon();
+a=zeros(6);
+a(2:3,2:3)=1;
+a(4:5,4:5)=1;
+
+function [a]= pulsar();
+a=zeros(17);
+a([5:7,11:13],[3,8,10,15])=1;
+a([3,8,10,15],[5:7,11:13])=1;
+
+function [a]=glider();
+a=zeros(5);
+a(2,3)=1;
+a(3,4)=1;
+a(4,2:4)=1;
+
+function [a]=LWSS();
+a=zeros(7);
+a(2,3:4)=1;
+a(3,2:5)=1;
+a(4,[2:3,5:6])=1;
+a(5,4:5)=1;
+
+function [a]=coe_ship();
+a=zeros(12);
+a(10,6:11)=1;
+a(9,[4:5,11])=1;
+a(8,[2:3,5,11])=1;
+a(7,[6,10])=1;
+a(6,8)=1;
+a(5,[8:9])=1;
+a(4,[7:10])=1;
+a(3,[7:8,10:11])=1;
+a(2,[9:10])=1;
+
+function [a]=turtle();
+a=zeros(14);
+a([2,11,12],1+[2:3,10:11])=1;
+a(3,1+[3:10])=1;
+a(5:6, 1+[3,10])=1;
+a([7,12],1+[5,8])=1;
+a(8,1+[3:4,6:7,9:10])=1;
+a(9,1+[4:5,8:9])=1;
+a(10,1+[2,4,9,11])=1;
+a(13,1+[6:7])=1;
+
+function [a]=schick();
+a=zeros(17);
+a([13:16],3+[2,10])=1;
+a([16,12],3+[3,9])=1;
+a([16,8],3+[4,8])=1;
+a([3:9,12,15],3+[5,7])=1;
+a([2,5:7,9:10],3+6)=1;
+
+function [a]=holzwart();
+a=zeros(13);
+a([2,12],1+[5,7:8])=1;
+a([3,11],1+[3,9])=1;
+a([4,10],1+[6:7,9:10])=1;
+a([5,9],1+[2:5,7:9])=1;
+a([6,8],1+8)=1;
+
+function [a]= glider_gun();
+a=zeros(11,38);
+a([6:7],[2:3])=1;
+a([4:5],[36:37])=1;
+a([6:8],[12,18])=1;
+a([5,9],[13,17])=1;
+a([4,10],[14:15])=1;
+a([7],[16,19])=1;
+a([4:6],[22:23])=1;
+a([3,7],[24])=1;
+a([2:3,7:8],[26])=1;
+
+function [a]=weekender()
+a=zeros(18);
+a(5:7,[7,12])=1;
+a(8,5:14)=1;
+a(9,[4,6,13,15])=1;
+a(10,[8,11])=1;
+a(11,[3:4,9:10,15:16])=1;
+a([12,14],[2:4,15:17])=1;
+a(13,[2,4,15,17])=1;
+
+function [a]=hickerson13491();
+a=zeros(17);
+a(2+4,2)=1;
+a(2+[2:4,7:9],3)=1;
+a(2+7,4)=1;
+a(2+[6,10:11],5)=1;
+a(2+[2:4,6:8,11],6)=1;
+a(2+[2:7,9:11],7)=1;
+a(2+[3:5],8)=1;
+a(:,[17:-1:10])=a(:,1:8);
+
+function [a]=hickerson13881();
+a=zeros(18);
+a(7,[15:16])=1;
+a(8,[3,4,8,12,17])=1;
+a(9,[2,9:10,13:16])=1;
+a(10,[3:5,7:8,10,12:13])=1;
+a(11,[7,9:10])=1;
+
+function [a]=glider17361();
+a=zeros(25);
+a(2,[12:13])=1;
+a(3,[11,14])=1;
+a(4,[11,13])=1;
+a(5,11)=1;
+a(6,[12:13,15])=1;
+a(7,[15:16,18])=1;
+a(8,[13,16,19,21])=1;
+a(9,[15:16,18:19,22])=1;
+a(10,[13,17:19,22])=1;
+a(11,[13,18])=1;
+a(12,[15,19])=1;
+a(13,16)=1;
+a(14,[15,18:22])=1;
+a(15,22)=1;
+a(16,[20,23:24])=1;
+a(18,[20:22])=1;
+a=a+a';
+
+function [a]=spider();
+a=zeros(29);
+a([3:4],[3:4])=1;
+a([3,5:8],6)=1;
+a(6,2)=1;
+a(7,[2:4,8:10])=1;
+a(8,[5,8,10,12:13])=1;
+a(9,11)=1;
+a([2,3,5],7)=1;
+a(6,8)=1;
+a([4:6],14)=1;
+a(:,29:-1:16)=a(:,1:14);
+
+function [a]=lindners();
+a=zeros(18);
+a(9,[2:4,7:12,15:17])=1;
+a([8,10],[4,7,12,15])=1;
+
+function [handles]=creat_pattern(handles);
+p_l=length(handles.pattern);
+if p_l > handles.M
+    pat_name=cellstr(get(handles.popupmenu1,'String'));
+    h=warndlg({'The grid size will be enlarged';['in order to fit to "',pat_name{get(handles.popupmenu1,'value')},'".']},'Grid size');
+    waitfor(h);
+    handles.M=p_l;
+    set(handles.M_size,'value',handles.M);
+    set(handles.sizeedit,'string',num2str(handles.M));
+    handles.vis_grid=handles.pattern;
+elseif (p_l<=handles.M)&(p_l>(.5*handles.M))
+    a=floor(1+.5*(handles.M - p_l));
+    handles.vis_grid=zeros(handles.M);
+    handles.vis_grid( a:a+p_l-1 , a:a+p_l-1 )=handles.pattern;
+else
+    lst_str={' one, in the corner',' one, in the center',' high period covered, same direction',' high period covered, random direction',' low period covered, same direction',' low period covered, random direction'};
+    [quest_lst,]=listdlg('ListString',lst_str,'ListSize',[230 85],'PromptString','How do you wish to organize the pattern?','SelectionMode','Single');
+    quest=lst_str{quest_lst};
+    switch quest
+        case ' one, in the corner'
+            handles.vis_grid=zeros(handles.M);
+            handles.vis_grid( 1:p_l,1:p_l )=handles.pattern;
+        case ' one, in the center'
+            a=floor(1+.5*(handles.M-p_l));
+            handles.vis_grid=zeros(handles.M);
+            handles.vis_grid( a:a+p_l-1 , a:a+p_l-1 )=handles.pattern;
+        case ' high period covered, same direction'
+            handles.vis_grid=zeros(handles.M);
+            for k=0:floor(handles.M/p_l)-1
+                for l=0:floor(handles.M/p_l)-1
+                    handles.vis_grid(1+k*p_l:(1+k)*p_l,1+l*p_l:(1+l)*p_l)=handles.pattern;
+                end %l
+            end %k
+        case ' high period covered, random direction'
+            handles.vis_grid=zeros(handles.M);
+            for k=0:floor(handles.M/p_l)-1
+                for l=0:floor(handles.M/p_l)-1
+                    if rand>.5
+                        o1=1:p_l;
+                    else
+                        o1=p_l:-1:1;
+                    end %if rand
+                    if rand>.5
+                        o2=1:p_l;
+                    else
+                        o2=p_l:-1:1;
+                    end %if rand
+                    if rand>.5
+                        pt=handles.pattern;
+                    else
+                        pt=handles.pattern';
+                    end %if rand
+                    handles.vis_grid(1+k*p_l:(1+k)*p_l,1+l*p_l:(1+l)*p_l)=pt(o1,o2);
+                end %l
+            end %k
+        case ' low period covered, same direction'
+            handles.vis_grid=zeros(handles.M);
+            for k=0:floor(handles.M/p_l)-1
+                for l=0:floor(handles.M/p_l)-1
+                    if ((k/2)==floor(k/2))&&((l/2)==floor(l/2))
+                        handles.vis_grid(1+k*p_l:(1+k)*p_l,1+l*p_l:(1+l)*p_l)=handles.pattern;
+                    end %if
+                end %l
+            end %k
+        case ' low period covered, random direction'
+            handles.vis_grid=zeros(handles.M);
+            handles.pattern(2*handles.M,2*handles.M)=0;
+            for k=0:floor(handles.M/p_l)-1
+                for l=0:floor(handles.M/p_l)-1
+                    if ((k/2)==floor(k/2))&&((l/2)==floor(l/2))
+                        if rand>.5
+                            o1=1:p_l;
+                        else
+                            o1=p_l:-1:1;
+                        end %if rand
+                        if rand>.5
+                            o2=1:p_l;
+                        else
+                            o2=p_l:-1:1;
+                        end %if rand
+                        if rand>.5
+                            pt=handles.pattern;
+                        else
+                            pt=handles.pattern';
+                        end %if rand
+                        handles.vis_grid(1+k*p_l:(1+k)*p_l,1+l*p_l:(1+l)*p_l)=pt(o1,o2);
+                    end %if ((k/2)==floor(k/2))&((l/2)==floor(l/2))
+                end %l
+            end %k
+    end %switch
+end %if
